89c81db45d55c12302407751597e5089e2ee2dd9
2525838 <c0a2525838@edu.teu.ac.jp>
2026-04-14 15:14:11 +0900

    練習３と４:こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -11,16 +11,36 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
-    screen.blit(bg_img,[300,200])
-    pg.display.update()
+
+    
+    k_img = pg.image.load("fig/3.png")
+    k_img = pg.transform.flip(k_img, True, False)
+
+    
+    bg_x = 0
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT:
+                return
 
-        screen.blit(bg_img, [0, 0])
+        
+        bg_x -= 2
+
+        
+        if bg_x <= -bg_img.get_width():
+            bg_x = 0
+
+        
+        screen.blit(bg_img, [bg_x, 0])
+        screen.blit(bg_img, [bg_x + bg_img.get_width(), 0])
+
+        
+        screen.blit(k_img, [300, 200])
+
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        clock.tick(200)
 
 
 if __name__ == "__main__":
